cd7b6cf3c9a68f18b79aba65f4b3f0863f7f5e5e
Javier Ramirez <apollo.infinity88@gmail.com>
2018-11-27 07:38:34 -0800

    created migrations for DB

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 
 class MyList(db.Model):
-    # __tablename__ = 'todo_items'
+    __tablename__ = 'todo_items'
     id = db.Column(db.Integer, primary_key=True)
     item = db.Column(db.Text, nullable=False)
     done = db.Column(db.Boolean, default=False)
