5022d2746c15d576ba21753e6013d015c64849b9
Javier Ramirez <apollo.infinity88@gmail.com>
2019-01-07 07:30:04 -0800

    Whitespace removed

--- todo.py
+++ todo.py
@@ -1,9 +1,11 @@
-from app import app, db # First app is the folder module, then the second app variable
+from app import app, db  # First app is the folder module, then the second app variable
 from app.models import MyList
+
 
 @app.shell_context_processor
 def make_shel_context():
     return {'db': db, 'MyList': MyList}
 
+
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug=True)
